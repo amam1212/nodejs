b17f5d4ee5a8e2a08e732a3916177df5ed949d92
AM Paruj <amparuj@gmail.com>
2019-12-18 12:04:51 +0700

    add try catch

--- src/decode.js
+++ src/decode.js
@@ -15,22 +15,35 @@ app.use(bodyParser.urlencoded({
 var server = new StellarSdk.Server('https://api.omchain.network/');
 
 router.post("/", (req, res) => {
-  var json = req.body;
-  var transaction = new StellarSdk.Transaction(json.xdr,'Omplatform Stellar Network ; April 2019');
-  let code = null;
-  if( transaction.memo.value) {
-    code = transaction.memo.value.toString()
+  try {
+
+
+    var json = req.body;
+    var transaction = new StellarSdk.Transaction(json.xdr, 'Omplatform Stellar Network ; April 2019');
+    let code = null;
+    if (transaction.memo.value) {
+      code = transaction.memo.value.toString()
+    }
+    var result = StellarSdk.Utils.verifyTxSignedBy(transaction, json.publicKey);
+
+
+    const data = {
+      'publicKey': json.publicKey,
+      'checkSignature': result,
+      'passcode': code,
+      'code': 200
+    }
+    res.json(data);
+  }catch (e) {
+
+
+    const data = {
+      'message': `XDR or Public Key are not incorrect`,
+      'code': 500
+    }
+    res.json(data);
+  
   }
-  var result =  StellarSdk.Utils.verifyTxSignedBy(transaction,json.publicKey);
-
-
-  const data = {
-    'publicKey': json.publicKey,
-    'checkSignature': result,
-    'code': code
-  }
-
-  res.json(data);
 });
 
 app.use(`/.netlify/functions/decode`, router);
